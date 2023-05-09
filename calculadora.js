8006dfe78eb6398dba3b639ec361e11892a8fafd
igornfaustino <igor@nfaustino.com>
2023-05-08 21:26:42 -0300

    add custom module example

---
+++ calculadora.js
@@ -0,0 +1,19 @@
+const soma = (a, b) => {
+    return a + b
+};
+const subtracao = (a, b) => {
+    return a - b
+};
+const mult = (a, b) => {
+    return a * b
+};
+const div = (a, b) => {
+    return a / b
+};
+
+module.exports = {
+    soma,
+    subtracao,
+    mult,
+    div
+}
---
+++ math-teste.js
@@ -0,0 +1,7 @@
+// import calculadora from "./calculadora";
+const calculadora = require("./calculadora");
+
+console.log("soma de 2 com 2: ", calculadora.soma(2,2))
+console.log("multiplicacao de 2 x 2: ", calculadora.mult(2,2))
+console.log("2 - 2: ", calculadora.subtracao(2,2))
+console.log("2 / 2: ", calculadora.div(2,2))
